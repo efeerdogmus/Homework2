4cca6f8da525d96a787da50123a00ac16963de87
mehmetbars <91800297+mehmetbars@users.noreply.github.com>
2023-09-22 19:38:50 +0300

    Update Homework2.java

--- Homework2.java
+++ Homework2.java
@@ -77,4 +77,29 @@ public class Homework2 {
 
         } while (chosenOption != 3);
     }
+        int[] avr;
+    public average (int[] arr){
+        int length = arr.length;
+        avr = new int[length];
+        double sum = 0;
+        int average;
+        for (int num : arr) {
+            sum += num;
+        }
+        average = (int)sum / length;
+        for (int i = 0; i < arr.length; i++){
+            avr[i] = average - arr[i];
+        }
+    }
+    public void display() {
+        System.out.print("{");
+        for (int i = 0; i < avr.length; i++) {
+            System.out.print(avr[i]);
+
+            if (i < avr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+    }
 }
